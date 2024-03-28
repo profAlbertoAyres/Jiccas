f44a5455c11ee8ae47afe21e18704b14cf15abbf
Alberto <alberto.benicio@ifro.edu.br>
2024-03-28 14:37:47 -0400

    Atualuzação para resposividade

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,11 @@
 var btnMenu = document.querySelector('#btn-menu');
 var menuSide = document.querySelector('.menu-lateral');
 var menuTexto = document.querySelectorAll('.menu-text');
+var menuResp = document.querySelector('.menu-resp');
+var screenWidth = window.screen.width;
 
+
+console.log("Resolução da tela: "+screenWidth);
 
 
 function aparecerTexto() {
@@ -15,7 +19,10 @@ btnMenu.addEventListener('click', function () {
         btnMenu.innerHTML='<i class="bi bi-list"></i>';
     else
         btnMenu.innerHTML = '<i class="bi bi-x-circle"></i>';
+    if(screenWidth=780){
 
+    }
+    menuResp.classList.toggle('menu-resp');
     menuSide.classList.toggle('mostar');
     aparecerTexto();
 });
